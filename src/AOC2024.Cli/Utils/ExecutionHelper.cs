4c019051264502390bf18b366d403907dcd2c2e4
Niklas Arndt <git@niklasarndt.com>
2024-12-21 15:01:20 +0100

    fix: Use correct runtime calculation

--- src/AOC2024.Cli/Utils/ExecutionHelper.cs
+++ src/AOC2024.Cli/Utils/ExecutionHelper.cs
@@ -6,40 +6,26 @@ namespace AOC2024.Cli.Utils;
 
 public class ExecutionHelper
 {
-
-    private static bool TryGetInputFile(DayBase solution, string folder, int part, out IEnumerable<string> input)
-    {
-        try
-        {
-            input = solution.ReadFile(folder, part);
-            return true;
-        }
-        catch (FileNotFoundException e)
-        {
-            Console.WriteLine($"Failed to load input for part {part}: {e.Message} ({e.GetType().Name})");
-            input = [];
-            return false;
-        }
-    }
-
     public static async Task ExecuteSolution(DayBase solution, string folder) 
     {
         try {
-            var stopwatch = Stopwatch.StartNew();
             var input = solution.ReadFile(folder, 1);
+            var stopwatch = Stopwatch.StartNew();
             var result = await solution.SolvePartOne(input);
             stopwatch.Stop();
-            Console.WriteLine($"Part 1: {result} (in {stopwatch.ElapsedMilliseconds:0.0} ms)");
+            var elapsedMs = stopwatch.ElapsedTicks / (Stopwatch.Frequency / 1000.0);
+            Console.WriteLine($"Part 1: {result} (in {elapsedMs:0.0} ms)");
         } catch (Exception e) {
             Console.WriteLine($"Part 1: {e.Message} ({e.GetType().Name})");
         }
 
         try {
-            var stopwatch = Stopwatch.StartNew();
             var input = solution.ReadFile(folder, 2);
+            var stopwatch = Stopwatch.StartNew();
             var result = await solution.SolvePartTwo(input);
             stopwatch.Stop();
-            Console.WriteLine($"Part 2: {result} (in {stopwatch.ElapsedMilliseconds:0.0} ms)");
+            var elapsedMs = stopwatch.ElapsedTicks / (Stopwatch.Frequency / 1000.0);
+            Console.WriteLine($"Part 2: {result} (in {elapsedMs:0.0} ms)");
         } catch (Exception e) {
             Console.WriteLine($"Part 2: {e.Message} ({e.GetType().Name})");
         }
